8056404d125f192b9ea0d765c0fb53f76b745db8
mroenca40 <mproenca.marketing@gmail.com>
2022-04-14 14:55:18 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Wine Catalogs</title>
+      </Head>
+      <h1>wine catalogs</h1>
+    </div>
+  )
+}
